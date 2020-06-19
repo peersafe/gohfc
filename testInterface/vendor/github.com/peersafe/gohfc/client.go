db4edc2d282404d07e3ce89e650c1a62dfbd1f72
zyf <zyf7862634@163.com>
2020-06-19 10:58:13 +0800

    feat - update exec  timeout time

--- testInterface/vendor/github.com/peersafe/gohfc/client.go
+++ testInterface/vendor/github.com/peersafe/gohfc/client.go
@@ -527,8 +527,8 @@ func (c *FabricClient) SyncInvoke(identity Identity, chainCode ChainCode, peers 
 		}
 	case err := <-reg.Errch:
 		return nil, err
-	case <-time.After(60 * time.Second):
-		return nil, fmt.Errorf("tx %s failed wait txstatus time out 30s", prop.transactionId)
+	case <-time.After(120 * time.Second):
+		return nil, fmt.Errorf("tx %s failed wait txstatus time out 120s", prop.transactionId)
 	}
 	return &InvokeResponse{Status: reply.Status, TxID: prop.transactionId}, nil
 }
